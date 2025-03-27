c2438c5a3afa89d88804e0ddcc69848a734acbdb
Dmitriy Voronin <devoronin@gmail.com>
2025-03-27 11:11:51 +0300

    Init

---
+++ HugsAndKisses/HugsAndKisses/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HugsAndKisses
+//
+//  Created by Dmitrii Voronin on 27.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
